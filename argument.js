1b9c199077ad3e78fb81add26c3dde2514e951ec
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-01 15:46:56 +0600

    function argument pass

---
+++ argument.js
@@ -0,0 +1,28 @@
+
+
+
+function double(number){ // (number) aii ta bola hoi parameter
+    const doubleleft=number*2;
+    console.log(doubleleft);
+}
+ double(15);//inside value called a aggument 
+ console.log('------------>');
+ double(20);
+ console.log('--------->');
+ const number=55;
+ double(number);
+
+ const price=110;
+ double(price);
+
+
+ console.log("fatmer and me math");
+ 
+ function difference(num1,num2){
+    const diff=num1-num2;
+    console.log(num1,num2 ,"age difference=",diff);
+
+ }
+ const fatherage=40;
+const myage=23;
+difference(fatherage,myage);
