40930aca7de28d23c5b31451ca15bad57e82be05
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-01 14:03:56 +0600

    introduction of sum function

---
+++ parameter.js
@@ -0,0 +1,28 @@
+function square(number){
+ console.log("value of the parameter",number);
+ const borgo=number*number;
+ console.log("square of the parameter is=",borgo);
+}
+
+square(4); //means function barbar use kora jai no problem okay
+square(5);
+square(6);
+square(7);
+console.log('another function');
+function add(num1,num2){
+    const sum=num1+num2;
+    console.log(sum);
+}
+add(3,2);
+add(4,10);
+console.log('multiple value add ar function');
+function addition(a,b,c,d,e){
+    const sum=a+b+c+d+e; //parameter only worked inside the function okay I think you understand properly
+    console.log(a,b,c,d,e);
+
+    console.log(sum);
+}
+addition(4,5,6,7,);
+
+
+
