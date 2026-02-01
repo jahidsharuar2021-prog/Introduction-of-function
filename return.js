3db5a7759122b0f66f231230cfe2bc262c515f57
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-01 16:14:22 +0600

    function return

---
+++ return.js
@@ -0,0 +1,9 @@
+function tentimes(number){
+    result=number*10;
+    return result;
+}
+const output=tentimes(990);
+console.log(output);
+console.log("--------->")
+const Jahid=tentimes(480);
+console.log(Jahid);
