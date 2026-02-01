9a0c2e1d2d68dea69fca9210b664495fbaafc96a
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-01 17:00:28 +0600

    is even or add

---
+++ conditional.js
@@ -0,0 +1,22 @@
+function iseven(number){
+    if(number%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(iseven(5));
+console.log('another example');
+console.log(iseven(10));
+
+//find the different between the number
+function diff(number1){
+    if(number1%2===1){
+        return true;
+    }
+    return  false;
+}
+console.log('another example');
+console.log(diff(5));
+console.log(diff(6));
--- return.js
+++ return.js
@@ -7,3 +7,19 @@ console.log(output);
 console.log("--------->")
 const Jahid=tentimes(480);
 console.log(Jahid);
+function recaptimes(number){
+    return number/2;
+}
+const hasan=recaptimes(40);
+console.log('short rules',hasan);
+
+// do math function 
+function domath(num1,num2){
+     const sum=num1+num2;
+    const different=num1-num2;
+    const muli=sum*different;
+    const res=muli/3;
+    return res;
+}
+const res=domath(4,6);
+console.log(res);
